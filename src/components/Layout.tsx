23fa6fca19b8afe5894547c1a8788706a24e0109
WuTangLan1 <finnmassari16@gmail.com>
2025-03-06 09:28:56 +0200

    removing unecessary content

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Header />
-      <Container maxWidth="lg" sx={{ py: { xs: 2, sm: 3, md: 4 } }}>
+      <Container maxWidth={false} sx={{ width: '100%', py: { xs: 2, sm: 3, md: 4 }, px: 0 }}>
         {children}
       </Container>
       <Footer />
